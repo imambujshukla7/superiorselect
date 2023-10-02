14c46e456de60e3853eacd97a95ce901744c0fc8
Ambuj Shukla <146608389+imambujshukla7@users.noreply.github.com>
2023-10-02 13:15:24 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,32 @@ import ProductList from './pages/ProductList';
 import ProductDetails from './pages/ProductDetails';
 import Breadcrumbs from './components/Breadcrumbs';
 
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./routes/productRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env
+
+const app = express();
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.use(express.json());
+
+
+app.use('/api', productRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 const App = () => {
   return (
     <Router>
